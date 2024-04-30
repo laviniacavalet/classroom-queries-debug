4a9f52fd88af630b12f595267d4e47799c667f69
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-30 05:32:30 +0000

    Add delete update department

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,16 +1,20 @@
 class DepartmentsController < ApplicationController
+
   def index
-    @departments = Department.all.order({ :created_at => :desc })
+    matching_departments = Department.all
+    @list_of_departments = matching_departments.order({ :created_at => :desc })
 
     render({ :template => "departments/index" })
-  end
+   end
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
-
+    matching_departments = Department.where({ :id => the_id })
+    @the_department = matching_departments.at(0)
+    
     render({ :template => "departments/show" })
   end
+
 
   def create
     @department = Department.new
